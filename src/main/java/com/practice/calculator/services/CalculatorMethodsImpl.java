bda8c2c7366321fa2ffd16cd6cf893aff4e88526
mjuezcas <mjcod7@gmail.com>
2023-05-02 23:50:22 +0200

    Include the jar trace in order to trace the result.

--- src/main/java/com/practice/calculator/services/CalculatorMethodsImpl.java
+++ src/main/java/com/practice/calculator/services/CalculatorMethodsImpl.java
@@ -1,37 +1,54 @@
 package com.practice.calculator.services;
 
-import com.practice.calculator.models.OperationParams;
+import io.corp.calculator.TracerImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorMethodsImpl implements ICalculatorMethods {
 
+    private TracerImpl tracer = new TracerImpl();
+
     @Override
     public Double sum(Double num1, Double num2) {
         System.out.println("Trying to sum "+ num1 + " and "+ num2);
-        return num1 + num2;
+        Double result = num1 + num2;
+        tracer.trace(result);
+        return result;
     }
 
     @Override
     public Double subtract(Double num1, Double num2) {
         System.out.println("Trying to subtract "+ num1 + " and "+ num2);
-        return num1 - num2;
+        Double result = num1 - num2;
+        tracer.trace(result);
+        return result;
     }
 
     @Override
     public Double operation(Double num1, Double num2, String operation) throws ArithmeticException {
+        Double result;
         switch(operation){
             case "+":
-                return num1 + num2;
+                System.out.println("Trying to sum "+ num1 + " and "+ num2);
+                result = num1 + num2;
+                break;
             case "-":
-                return num1 - num2;
+                System.out.println("Trying to subtract "+ num1 + " and "+ num2);
+                result = num1 - num2;
+                break;
             case "*":
-                return num1 * num2;
+                System.out.println("Trying to multiply "+ num1 + " and "+ num2);
+                result = num1 * num2;
+                break;
             case "/":
-                return num1 / num2;
+                System.out.println("Trying to divide "+ num1 + " and "+ num2);
+                result = num1 / num2;
+                break;
             default:
                 System.out.println("You send a operation not allowed");
                 throw new ArithmeticException();
         }
+        tracer.trace(result);
+        return result;
     }
 }
